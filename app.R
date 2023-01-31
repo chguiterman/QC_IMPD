4bfb9bf6efc3f3ae99de7a00e8400e07d478f6ff
Chris Guiterman <chguiterman@email.arizona.edu>
2023-01-30 20:40:06 -0500

    huge progress

--- app.R
+++ app.R
@@ -50,8 +50,9 @@ ui <- fluidPage(
     mainPanel(
       includeMarkdown("text_intro.Rmd"),
       # Output: Data file ----
-      tableOutput("contents")
-
+      tableOutput("contents"),
+      # Download RMD
+      downloadButton("report", "Generate report")
      )
     
   )
@@ -65,17 +66,59 @@ server <- function(input, output) {
   meta_upload <- eventReactive(input$meta, {
     meta_in <- input$meta
     meta_ext <- file_ext(meta_in$name)
-    if (meta_ext == ".csv") {
+    if (meta_ext == "csv") {
       read_csv(meta_in$datapath)
     } else  read_excel(meta_in$datapath)
   }
   )
-                           
+  
+  fhx_upload <- eventReactive(input$fhx, {
+    data.frame(file_name = input$fhx$name) %>% 
+      mutate(FHX = map(input$fhx$datapath, ~read_fhx(.x, verbose=FALSE)),
+             n_trees = map_dbl(FHX, ~length(series_names(.x))))
+    
+ })                           
 
+  comb_dat <- reactive(
+    inner_join(meta_upload(), fhx_upload(), 
+               by = c("FHX_FILENAME" = "file_name")
+    )
+  )
 
 output$contents <- renderTable({
-  meta_upload()
+  # meta_upload()[, c("NAME_OF_SITE", "SITE_CODE")]
+  # fhx_upload() %>% 
+  #   select(file_name, n_trees)
+  comb_dat() %>% 
+    select(SITE_CODE, n_trees)
   })
+
+# https://bookdown.org/yihui/rmarkdown-cookbook/child-document.html
+# https://shiny.rstudio.com/articles/generating-reports.html
+# https://bookdown.org/yihui/rmarkdown-cookbook/dynamic-yaml.html
+
+output$report <- downloadHandler(
+  # For PDF output, change this to "report.pdf"
+  filename = "QC_report.html",
+  content = function(file) {
+    # Copy the report file to a temporary directory before processing it, in
+    # case we don't have write permissions to the current working dir (which
+    # can happen when deployed).
+    tempReport <- file.path(tempdir(), "QC_report.Rmd")
+    file.copy("QC_report.Rmd", tempReport, overwrite = TRUE)
+    
+    # Set up parameters to pass to Rmd document
+    params <- list(FHX = comb_dat()$FHX[[1]])
+    
+    # Knit the document, passing in the `params` list, and eval it in a
+    # child of the global environment (this isolates the code in the document
+    # from the code in this app).
+    rmarkdown::render(tempReport, output_file = file,
+                      params = params,
+                      envir = new.env(parent = globalenv())
+    )
+  }
+)
 }
 
 # Run the application 
